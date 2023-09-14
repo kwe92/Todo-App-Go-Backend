b5236e89c1a723a69a7b1749074323acad32e963
Kwe <Kweayon@gmail.com>
2023-09-13 22:30:33 -0500

    update: updated json serialization keys

--- declarations/declarations.go
+++ declarations/declarations.go
@@ -4,9 +4,9 @@ import "net/http"
 
 type Task struct {
 	ID          string `json:"id"`
-	TaskName    string `json:"taskName"`
-	TaskDetails string `json:"taskDetails"`
-	CreatedDate string `json:"createdDate"`
+	TaskName    string `json:"task_name"`
+	TaskDetails string `json:"task_details"`
+	CreatedDate string `json:"created_date"`
 }
 
 type HeaderMetaData struct {
