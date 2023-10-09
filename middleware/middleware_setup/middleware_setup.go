f659e9f7e3653b527ba8fe807de40e4c6bb61e6f
Kwe <Kweayon@gmail.com>
2023-10-09 12:45:08 -0500

    update: added comments and made headers optional

--- middleware/middleware_setup/middleware_setup.go
+++ middleware/middleware_setup/middleware_setup.go
@@ -6,11 +6,17 @@ import (
 	responseheader "response_header"
 )
 
-// TODO: add coments
+// SetupMiddleware: wraps the handler passed in with middleware handlers.
+func SetupMiddleware(handler http.Handler, headers ...map[string]string) http.Handler {
 
-// TODO: change implementation to set headers dynamically
-
-func SetupMiddleware(handler http.Handler) http.Handler {
-
+	if len(headers) > 0 {
+		var key string
+		var value string
+		for k, v := range headers[0] {
+			key = k
+			value = v
+		}
+		return logger.NewLogger(responseheader.NewResponseHeader(handler, key, value))
+	}
 	return logger.NewLogger(responseheader.NewResponseHeader(handler, "Content-Type", "application/json"))
 }
