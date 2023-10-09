02f60f5954b1bb4bc749c670e0d0ba53c358e946
Kwe <Kweayon@gmail.com>
2023-10-09 14:08:42 -0500

    update

--- utilities/utilities.go
+++ utilities/utilities.go
@@ -28,13 +28,13 @@ func GetId() string {
 }
 
 // jsonEncode converts [data] to a JSON string and sends it over the stream as a response.
-func JsonEncode[T any](w http.ResponseWriter, data T) {
-	json.NewEncoder(w).Encode(data)
+func JsonEncode[T any](w http.ResponseWriter, data T) error {
+	return json.NewEncoder(w).Encode(data)
 }
 
 // JsonDecode reads the next JSON-encoded value from its input and stores it in the value pointed to by v.
-func JsonDecode[T any](rc io.ReadCloser, ptr *T) {
-	json.NewDecoder(rc).Decode(&ptr)
+func JsonDecode[T any](rc io.ReadCloser, ptr *T) error {
+	return json.NewDecoder(rc).Decode(&ptr)
 }
 
 // setHeader sets up HTTP Header meta data.
